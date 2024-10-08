362669897bcbe2dbf6c920f9ac8a23d8ca0857b4
samvura <samvura.mpofu@gmail.com>
2024-10-08 06:37:52 +0100

    homepage setup and register user functions

---
+++ accounts/views.py
@@ -0,0 +1,56 @@
+from django.http.response import HttpResponse
+from django.shortcuts import redirect,render
+
+#from vendor.forms import VendorForm
+from .forms import UserForm
+from .models import User, UserProfile
+from django.contrib import messages, auth
+#from .utils import detectUser, send_verification_email
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+
+# Create your views here.
+
+def registerUser(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in!')
+        return redirect('dashboard')
+    elif request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # Create the user using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+
+            # Create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+
+            # Send verification email
+            # mail_subject = 'Please activate your account'
+            # email_template = 'accounts/emails/account_verification_email.html'
+            # send_verification_email(request, user, mail_subject, email_template)
+            messages.success(request, 'Your account has been registered sucessfully!')
+            return redirect('registerUser')
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
+
+
+
+
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -15,10 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+#from marketplace import views as MarketplaceViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+    path('accounts/', include('accounts.urls')),
+
+    # path('marketplace/', include('marketplace.urls')),
+
+    # # CART
+    # path('cart/', MarketplaceViews.cart, name='cart'),
+    # # SEARCH
+    # path('search/', MarketplaceViews.search, name='search'),
+
+    # # CHECKOUT
+    # path('checkout/', MarketplaceViews.checkout, name='checkout'),
+
+    # # ORDERS
+    # path('orders/', include('orders.urls')),    
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
